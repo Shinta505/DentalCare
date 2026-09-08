e3017c3a9f00c53fb86f451b396c6fd1c2fc9c98
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2026-09-09 06:38:02 +0700

    Refactor database connection to use DATABASE_URL

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -4,13 +4,7 @@ import pg from "pg";
 
 dotenv.config();
 
-const DB_NAME = process.env.DB_NAME;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-
-const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
+const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectModule: pg,
   dialectOptions: {
